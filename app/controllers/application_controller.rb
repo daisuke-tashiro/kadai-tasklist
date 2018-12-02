a169af2b86d36e3fb73401daedac653045ff0af4
daisuke.tashiro <makeyourlifeexciting@gmail.com>
2018-12-02 06:19:28 +0000

    message model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def index
+  end 
+  
+  def show 
+  end 
+  
+  def new
+  end 
+  
+  def create
+  end 
+  
+  def edit
+  end 
+  
+  def update
+  end 
+  
+  def destroy
+  end 
+  
 end
